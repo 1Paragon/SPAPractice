bb47b88c267803fe4fda601c521b8a9d4bb380b9
1Paragon <1Paragon@protonmail.com>
2020-10-27 21:19:36 -0700

    additions

--- server/echo.js
+++
@@ -1,9 +0,0 @@
-const server = http.createServer((request, response) => {
-  //console.log(request.url);
-  if (request.url ==== "/") { "Content-Type": "application/json" });
-  response.write(JSON.stringify ({ message: Welcome to the main page }));
-  response.end();
-  }
-});
-server.listen(404);
-console.log(`Listening on Port: ${4040}`);
